c2dda34491e18676b4a1a74a50b9ef547ffeb660
bebofofum <bri.boehm@gmail.com>
2021-03-03 22:47:27 -0500

    commit tree 1bcd04cec2ce553e96e7de0ad3b7df9a9d40cc90

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
 
 class CreateTodo extends Component {
+
+    state = {
+      text: ''
+    }
+
+
+
+  handleClick = event => {
+    this.setState({
+        text: event.target.value
+    });
+
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // when this is called, whatever is in this.state is sent off to our reducer via dispatch
+    console.log('State', this.state)
+    // we say this.props.addTodo because addToDo is declared as a prop below in the maptodispatch function below
+    // if we didn't declare addtodo as a prop below we could write this as this.props.dispatch({type: 'ADD_TODO', payload: formData})
+    this.props.addTodo(this.state)
+  }
+
+
   render() {
     return(
       <div>
-        Create Todo Component
+        
+          <form onSubmit={event => this.handleSubmit(event) }>
+            <p>
+              <label>add todo</label>
+              <input 
+                type='text' 
+                placeholder='monkeys' 
+                value={this.state.text} 
+                onChange={this.handleClick} />
+            </p>
+            <input 
+                type="submit" 
+                value="Add Todo"  />
+          </form>
+          state now = {this.state.text}
+
       </div>
     )
   }
 }
 
-export default CreateTodo;
+// mapDispatchToProps should be declared like a function expression
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // addTodo with below code becomes a prop
+    addTodo: formData => dispatch(
+      { 
+        type: 'ADD_TODO', 
+        payload: formData })
+  }
+
+}
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
